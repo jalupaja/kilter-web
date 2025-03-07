afc99ba34665b042b063f61deb89add15f8b7c6e
tresonic <37078118+tresonic@users.noreply.github.com>
2025-03-07 08:37:25 +0000

    remove junk

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,10 @@
-import { useState } from 'preact/hooks';
-import preactLogo from './assets/preact.svg';
-import viteLogo from '/vite.svg';
+// import { useState } from 'preact/hooks';
+// import preactLogo from './assets/preact.svg';
+// import viteLogo from '/vite.svg';
 // import { requestDevice } from './bluetooth';
 import './app.css';
 
 export function App() {
-    const [count, setCount] = useState<number>(333);
-
     // copied from bluetooth.js
     const SERVICE_UUID = "6e400001-b5a3-f393-e0a9-e50e24dcca9e";
     const CHARACTERISTIC_UUID = "6e400002-b5a3-f393-e0a9-e50e24dcca9e";
@@ -40,9 +38,7 @@ export function App() {
         return result;
     }
 
-    async function test(num: number): Promise<void> {
-        setCount(count + num);
-
+    async function test(): Promise<void> {
         const bluetoothPacket: number[] = [1, 37, 21, 2, 84, 227, 0, 28, 20, 1, 28, 69, 0, 31, 145, 0, 31, 63, 0, 31, 157, 0, 31, 208, 0, 31, 55, 1, 227, 27, 1, 244, 239, 0, 244, 33, 0, 244, 49, 1, 244, 3];
 
         const device = await navigator.bluetooth.requestDevice({
@@ -78,41 +74,14 @@ export function App() {
                 }
             });
 
-    //     // console.log()
     }
 
     return (
         <>
-            <div>
-                <a href="https://vite.dev" target="_blank">
-                    <img src={viteLogo} class="logo" alt="Vite logo" />
-                </a>
-                <a href="https://preactjs.com" target="_blank">
-                    <img src={preactLogo} class="logo preact" alt="Preact logo" />
-                </a>
-            </div>
-            <h1>Vite + Preact</h1>
-            <div class="card">
-                <button onClick={() => test(1)}>
-                    count is {count}
-                </button>
-                <p>
-                    Edit <code>src/app.tsx</code> and save to test HMR
-                </p>
-            </div>
-            <p>
-                Check out{' '}
-                <a
-                    href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-                    target="_blank"
-                >
-                    create-preact
-                </a>
-                , the official Preact + Vite starter
-            </p>
-            <p class="read-the-docs">
-                Click on the Vite and Preact logos to learn more
-            </p>
+            <h1>Magic Kilterboard Button</h1>
+            <button onClick={test}>
+                    Click here
+            </button>
         </>
     );
 }
